b80bc1f5273c58a42bd10a96569c111f1b50b009
hzlsrdz <107729764+hzlsrdz@users.noreply.github.com>
2024-07-03 13:45:33 -0700

    rearrange some stuff

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', subtitle='Home Page', text='This is the home page')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
