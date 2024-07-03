0a082b3ebaa1be21ae822a4b527f90403c8ab3cf
hzlsrdz <107729764+hzlsrdz@users.noreply.github.com>
2024-07-03 13:29:04 -0700

    very basic site

--- home.py
+++ home.py
@@ -1,9 +1,10 @@
-from flask import Flask
-app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
+from flask import Flask, render_template
+app = Flask(__name__)
 
-@app.route("/")                          # this tells you the URL the method below is related to
-def hello_world():
-    return "<p>Hello, World!</p>"        # this prints HTML to the webpage
-  
-if __name__ == '__main__':               # this should always be at the end
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', subtitle='Home Page', text='This is the home page')
+
+if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
